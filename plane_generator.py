cbac5497541c8d8e1e7c36ee21d54cc55914041a
kronek <kronkowski.daniel@gmail.com>
2023-10-07 18:53:58 +0200

    inital file

---
+++ plane_generator.py
@@ -0,0 +1,16 @@
+class PlaneGenerator:
+
+    def __init__(self):
+        pass
+
+
+    def vector_generator(self):
+        pass
+
+
+    def position_generator(self):
+        pass
+
+
+    def new_plane():
+        pass
---
+++ plane_manager.py
@@ -0,0 +1,2 @@
+'''Manage planes script'''
+
