6b6c56e23f5007bae2ebc9c8bcca60f63dd20e6f
kronek <kronkowski.daniel@gmail.com>
2023-10-07 18:55:04 +0200

    #3 add PlaneSocket class

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
-'''Client socket API'''
-
 import socket as s
+import time
+import random
+from plane_generator import PlaneGenerator
 
 
 class PlaneSocket:
@@ -8,4 +9,20 @@ class PlaneSocket:
     PORT = 65432
 
     def __init__(self):
+        self.client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+        self.client_socket.connect((self.HOST, self.PORT))
+            
+
+    def send_socket(self):
+        while True:
+            command = PlaneGenerator().new_plane().encode('utf8')
+            self.client_socket.sendall(command)
+            data = self.client_socket.recv(1024).decode('utf8')
+            print(data)
+            time.sleep(random.randint(1,7))
+
+
+if __name__ == '__main__':
+    client = PlaneSocket()
+    client.send_socket()
 
