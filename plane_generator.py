9c9ea8e5aa62e14cb567b5bc06f500e06980c2cf
kronek <kronkowski.daniel@gmail.com>
2023-10-08 14:48:28 +0200

    #4 add plane info generator script

--- plane_generator.py
+++ plane_generator.py
@@ -1,16 +1,49 @@
+import random as r
+
+
 class PlaneGenerator:
 
     def __init__(self):
-        pass
-
-
-    def vector_generator(self):
-        pass
-
+        self.direction = r.choice(['NNE','ENE','NNW','WNW','SSE','ESE','SSW','WSW'])
+        self.velocity = r.randint(200,300)
 
     def position_generator(self):
-        pass
+        match self.direction:
+            case'NNE':
+                position = [r.randint(0,5000), r.randint(4500,5000), r.randint(500,2000)]
+            case'ENE':
+                position = [r.randint(4500,5000), r.randint(0,5000), r.randint(500,2000)]
+            case'NNW':
+                position = [-r.randint(5000,0), r.randint(4500,5000), r.randint(500,2000)]
+            case'WNW':
+                position = [-r.randint(4500,5000), r.randint(0,5000), r.randint(500,2000)]
+            case'SSE':
+                position = [r.randint(0,5000), -r.randint(4500,5000), r.randint(500,2000)]
+            case'ESE':
+                position = [r.randint(4500,5000), -r.randint(0,5000), r.randint(500,2000)]
+            case'SSW':
+                position = [-r.randint(5000,0), -r.randint(4500,5000), r.randint(500,2000)]
+            case'WSW':
+                position = [-r.randint(4500,5000), -r.randint(0,5000), r.randint(500,2000)]
+        return position
+
+    def vector_generator(self):
+        self.velocity_vector_x = r.randint(0,self.velocity)
+        match self.direction:
+            case'NNE' | 'ENE':
+                vector = [-self.velocity_vector_x, -(self.velocity-self.velocity_vector_x), 0]
+            case'NNW' | 'WNW':
+                vector = [self.velocity_vector_x, -(self.velocity-self.velocity_vector_x), 0]
+            case'SSE' | 'ESE':
+                vector = [-self.velocity_vector_x, (self.velocity-self.velocity_vector_x), 0]
+            case'SSW' | 'WSW':
+                vector = [self.velocity_vector_x, (self.velocity-self.velocity_vector_x), 0]
+        return vector
 
 
-    def new_plane():
-        pass
+    def flight_number_generator(self):
+        num = 1
+        while True:
+            yield num
+            num += 1
+
