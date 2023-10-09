aa96b2280d05aeafa3f47897079c8befb25bff19
kronek <kronkowski.daniel@gmail.com>
2023-10-09 21:38:10 +0200

    #4 add .env load and small changes in new_plane function

--- plane_manager.py
+++ plane_manager.py
@@ -1,19 +1,28 @@
 '''Manage planes script'''
 
+import os
+from dotenv import load_dotenv
 from plane_generator import PlaneGenerator
 from db_manager import DatabaseManager
+
+load_dotenv()
+
 
 class PlaneManager:
     
     def __init__(self):
-        self.flight_number = next(PlaneGenerator().flight_number_generator())
+        postgres_config_str = os.getenv('POSTGRES_CONFIG')
+        self.postgres_config = eval(postgres_config_str)
+        self.flight_number = PlaneGenerator().flight_number_generator()
         self.position = PlaneGenerator().position_generator()
         self.position_x = self.position[0]
         self.position_y = self.position[1]
         self.position_z = self.position[2]
         self.velocity = PlaneGenerator().vector_generator()
-
+        self.db = DatabaseManager(**self.postgres_config)
 
     def new_plane(self):
-        result = DatabaseManager().add_plane(self.flight_number,'IN_AIR',self.position_x, self.position_y, self.position_z, self.velocity)
+        result = self.db.add_plane(next(self.flight_number),'IN_AIR',self.position_x, self.position_y, self.position_z, self.velocity)
         return result
+    
+    
