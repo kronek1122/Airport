fe6f0c1c365a66f9622ff61c93bfbb2dd24abb19
kronek <kronkowski.daniel@gmail.com>
2023-10-11 20:18:24 +0200

    #3 fix flight_num generator - move it to client.py

--- client.py
+++ client.py
@@ -10,12 +10,18 @@ class PlaneSocket:
     def __init__(self):
         self.client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
         self.client_socket.connect((self.HOST, self.PORT))
-            
+        self.flight_number_gen = self.flight_number_generator()
+    
+    def flight_number_generator(self):
+        num = 1
+        while True:
+            yield num
+            num += 1
 
     def send_socket(self):
         while True:
-            command = 'new plane'
-            self.client_socket.sendall(command.encode('utf8'))
+            flight_num = next(self.flight_number_gen)
+            self.client_socket.sendall(str(flight_num).encode('utf8'))
             data = self.client_socket.recv(1024).decode('utf8')
             print(data)
             time.sleep(random.randint(1,7))
