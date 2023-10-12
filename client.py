024ad93b02271483b83bb9c1e801f5a9a62753e2
kronek <kronkowski.daniel@gmail.com>
2023-10-12 20:34:43 +0200

    mod __init__ and send_socket(), add def json_data_converter(), vector_update(), position_update()

--- client.py
+++ client.py
@@ -1,33 +1,61 @@
 import socket as s
 import time
-import random
-
+import json
+from plane_generator import PlaneGenerator
 
 class PlaneSocket:
     HOST = '127.0.0.1'
     PORT = 65432
 
-    def __init__(self):
+    def __init__(self, flight_num):
         self.client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
         self.client_socket.connect((self.HOST, self.PORT))
-        self.flight_number_gen = self.flight_number_generator()
-    
-    def flight_number_generator(self):
-        num = 1
-        while True:
-            yield num
-            num += 1
+        self.in_air = True
+        self.flight_num = flight_num
+        self.position = PlaneGenerator().position_generator()
+        self.position_x = self.position[0]
+        self.position_y = self.position[1]
+        self.position_z = self.position[2]
+        self.velocity = PlaneGenerator().vector_generator()
+
+
+    def json_data_converter(self):
+        flight_data = {"position x":self.position_x,
+                         "position y":self.position_y,
+                         "position_z":self.position_z,
+                         "velocity_vector":self.velocity}
+        self.json_flight_data = json.dumps(flight_data)
+        return self.json_flight_data
+
+
+    def vector_update(self, data):
+        self.velocity = data.get("velocity _vector")
+
+
+    def position_update(self):
+        self.position_x += self.velocity[0]
+        self.position_y += self.velocity[1]
+        self.position_z += self.velocity[2]
+
 
     def send_socket(self):
-        while True:
-            flight_num = next(self.flight_number_gen)
-            self.client_socket.sendall(str(flight_num).encode('utf8'))
-            data = self.client_socket.recv(1024).decode('utf8')
-            print(data)
-            time.sleep(random.randint(1,7))
+        while self.in_air == True:
+            flight_data = self.json_data_converter()
+            self.client_socket.sendall(flight_data.encode('utf8'))
+            received_data = self.client_socket.recv(1024).decode('utf8')
 
+            if received_data == 'to many planes in the air':
+                print(received_data)
+                break
+            else:
+                received_dict = json.loads(received_data)
+                self.vector_update(received_dict)
+                self.position_update()
 
-if __name__ == '__main__':
-    client = PlaneSocket()
-    client.send_socket()
+                if self.position_z == 0:
+                    flight_data = 'plane landed'
+                    self.client_socket.sendall(flight_data.encode('utf8'))
+                    print(flight_data)
+                    break
 
+            time.sleep(1)
