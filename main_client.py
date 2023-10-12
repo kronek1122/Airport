54b97b70350e6a70a2f77601c2dccead83081b32
kronek <kronkowski.daniel@gmail.com>
2023-10-12 20:35:51 +0200

    #3 add main_client - move run and flight_num_gen() to it

---
+++ main_client.py
@@ -0,0 +1,17 @@
+import time
+import random
+from client import PlaneSocket
+
+def flight_number_generator():
+        num = 1
+        while True:
+            yield num
+            num += 1
+
+flight_num_gen = flight_number_generator()
+
+while True:
+    flight_num = next(flight_num_gen)
+    client = PlaneSocket(flight_num)
+    client.send_socket()
+    time.sleep(random.randint(1,7))
