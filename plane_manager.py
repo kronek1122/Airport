63b67fc3cbdff9ffd8bea334454c185b12f1a96f
kronek <kronkowski.daniel@gmail.com>
2023-10-13 22:37:26 +0200

    #4 change inital variables

--- plane_manager.py
+++ plane_manager.py
@@ -2,7 +2,6 @@
 
 import os
 from dotenv import load_dotenv
-from plane_generator import PlaneGenerator
 from db_manager import DatabaseManager
 
 load_dotenv()
@@ -10,18 +9,18 @@ load_dotenv()
 
 class PlaneManager:
     
-    def __init__(self, flight_number):
-        postgres_config_str = os.getenv('POSTGRES_CONFIG')
+    def __init__(self, dictionary):
+        postgres_config_str = os.getenv('POSTGRES_CONFIG_DB')
         self.postgres_config = eval(postgres_config_str)
-        self.flight_number = int(flight_number)
-        self.position = PlaneGenerator().position_generator()
-        self.position_x = self.position[0]
-        self.position_y = self.position[1]
-        self.position_z = self.position[2]
-        self.velocity = PlaneGenerator().vector_generator()
+        self.flight_number = dictionary['flight_number']
+        self.position_x = dictionary['position x']
+        self.position_y = dictionary['position y']
+        self.position_z = dictionary['position_z']
+        self.velocity = dictionary['velocity_vector']
         self.db = DatabaseManager(**self.postgres_config)
 
-    def new_plane(self):
+
+    def plane_signal(self):
         result = self.db.add_plane(self.flight_number,'IN_AIR',self.position_x, self.position_y, self.position_z, self.velocity)
         return result
     
