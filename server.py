393a78e8ba8d727820133e952e7e7d674cb3a992
kronek <kronkowski.daniel@gmail.com>
2023-10-13 22:38:17 +0200

    #4 add json_data_converter, mod loop

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 import socket as s
+import json
 from plane_manager import PlaneManager
 
 class Server:
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, info):
         self.host = host
         self.port = port
+        self.info = info
         self.server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen()
 
-
-    def json_unpacking(self, data):
-        unpacking_data = []
-        unpacking_data = data.split(' ')
-        return unpacking_data
+    def json_data_converter(self, data):
+        self.json_flight_data = json.dumps(data)
+        return self.json_flight_data
 
     def run(self):
         connection, address = self.server_socket.accept()
@@ -22,8 +22,7 @@ class Server:
 
         while True:
             query = connection.recv(1024).decode('utf8')
-
-            if not query:
-                break
-        
-            connection.send(PlaneManager(query).new_plane().encode('utf8'))
+            received_dict = json.loads(query)
+            data = PlaneManager(received_dict).plane_signal()
+            json_data = self.json_data_converter(data)
+            connection.sendall(json_data.encode('utf8'))
