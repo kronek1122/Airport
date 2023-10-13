81728f48eee2bd82fe98a23dc6913b7ff90fcfcc
kronek <kronkowski.daniel@gmail.com>
2023-10-13 22:35:37 +0200

    #4 add INFO class Server call

--- main_server.py
+++ main_server.py
@@ -3,6 +3,6 @@ from server import Server
 HOST = '127.0.0.1'
 PORT = 65432
 INFO = 'version: 0.0.1; creation date: 30.09.2023r'
-server = Server(HOST,PORT)
+server = Server(HOST,PORT,INFO)
 
 server.run()
