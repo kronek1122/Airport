5c7586133bdc0ceeb5ad815519864d1b534393ee
kronek <kronkowski.daniel@gmail.com>
2023-10-15 22:09:31 +0200

    #4 exand plane_signal function (logic), add control_tower_system(), dictionary_data_pack()

--- plane_manager.py
+++ plane_manager.py
@@ -21,7 +21,34 @@ class PlaneManager:
 
 
     def plane_signal(self):
-        result = self.db.add_plane(self.flight_number,'IN_AIR',self.position_x, self.position_y, self.position_z, self.velocity)
+        if self.position_z == 0:
+            self.db.change_status(self.flight_number, 'LANDED')
+            msg = f'flight number {self.flight_number} landed'
+            print(msg)
+            result = 'landed'
+        else:
+            if self.db.num_of_plane_over_the_airport() < 100:
+                msg = self.db.add_plane(self.flight_number,'IN_AIR',self.position_x, self.position_y, self.position_z, self.velocity)
+                if msg == 'Error adding new plane to db':
+                    self.db.change_velocity_vector(self.flight_number,self.velocity)
+                    self.db.mod_plane_position(self.flight_number, self.position_x, self.position_y, self.position_z)
+                    result = self.control_tower_system()
+                else:
+                    result = self.control_tower_system()
+            else: 
+                result = 'to many planes in the air'
         return result
+
+
+    def control_tower_system(self):
+        #dodać system sterowanie ruchem
+        result = self.dictionary_data_pack()
+        return result
+
+
+    def dictionary_data_pack(self):
+        result = {'velocity_vector':self.velocity}
+        return result
+
     
     
