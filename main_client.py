d1999bcbc2156812bab597db88e6d51ba644eedd
kronek <kronkowski.daniel@gmail.com>
2023-10-16 19:59:57 +0200

    #3 add threading and create_client()

--- main_client.py
+++ main_client.py
@@ -1,4 +1,5 @@
 import time
+import threading
 from client import PlaneSocket
 
 def flight_number_generator():
@@ -7,10 +8,14 @@ def flight_number_generator():
             yield num
             num += 1
 
+def create_client(flight_num):
+    client = PlaneSocket(flight_num)
+    client.send_socket()
+
 flight_num_gen = flight_number_generator()
 
 while True:
     flight_num = next(flight_num_gen)
-    client = PlaneSocket(flight_num)
-    client.send_socket()
+    thread = threading.Thread(target=create_client, args=(flight_num,))
+    thread.start()
     time.sleep(1)
