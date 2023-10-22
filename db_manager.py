401e472c96414f83b2a9d30e46bfa2f72c12b650
kronek <kronkowski.daniel@gmail.com>
2023-10-22 18:37:13 +0200

    #2 mod query to sql in all funct, change msg from function to dictionary, add finally block to try-except - move realese_connection()

--- db_manager.py
+++ db_manager.py
@@ -8,57 +8,70 @@ class DatabaseManager:
         self.connection_pool = ConnectionPool(database, user, password, host)
 
     
-    def add_plane(self, flight_number, status, x, y, z, vector):
-        query = '''INSERT INTO flights_log (flight_number, status, x, y, z, velocity_vector)
-        VALUES (%s, %s, %s, %s, %s, %s)'''
-
-        values = (flight_number, status, x, y, z, vector)
+    def add_plane(self, flight_number:int, status:str, x:int, y:int, z:int, vector_x:int, vector_y:int, vector_z:int):
+        query = '''INSERT INTO flights_log (flight_number, status, x, y, z, velocity_vector_x, velocity_vector_y, velocity_vector_z)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'''
 
         try:
             conn = self.connection_pool.get_connection()
             curr = conn.cursor()
-            curr.execute(query, values)
+            values = (flight_number, status, x, y, z, vector_x, vector_y, vector_z)
+            curr.execute(query,values)
             conn.commit()
-            msg = 'New plane over the airport'
-            self.connection_pool.release_connection(conn)
-        except psycopg2.IntegrityError:
+            msg = {'msg': 'New plane over the airport',
+                   'error': False}
+        except psycopg2.IntegrityError as error:
             conn.rollback()
-            msg = 'Error adding new plane to db'
+            msg = {'msg': 'Error adding new plane to db',
+                   'error': True,
+                   'error_msg': f"{error}"}
+        finally:
+            self.connection_pool.release_connection(conn)
         return msg
 
 
-    def change_status(self, flight_number, status):
-        query = f'''UPDATE flights_log 
-        SET status = {status} 
-        WHERE flight_number = {flight_number};'''
+    def change_status(self, flight_number:int, status:str):
+        query = '''UPDATE flights_log 
+        SET status = %s 
+        WHERE flight_number = %s;'''
         try:
             conn = self.connection_pool.get_connection()
             curr = conn.cursor()
-            curr.execute(query)
+            curr.execute(query,(status,flight_number))
             conn.commit()
-            self.connection_pool.release_connection(conn)
+            msg = {'msg': 'Changed status',
+                   'error': False}
         except psycopg2.Error as error:
             conn.rollback()
-            msg = f'Error changing status: {error}'
-            return msg
+            msg = {'msg': 'Error changing status:',
+                   'error': True,
+                   'error_msg': f"{error}"}
+        finally:
+            self.connection_pool.release_connection(conn)
+        return msg
 
 
-    def change_plane_position_and_vector(self, flight_number, x, y, z, vector):
-        query = f'''UPDATE flights_log
-        SET x = {x}, y = {y}, z = {z} , velocity_vector = {vector}
-        WHERE flight_number = {flight_number};'''
+    def change_plane_position_and_vector(self, flight_number:int, x:int, y:int, z:int, vector_x:int, vector_y:int, vector_z:int)-> dict:
+        query = '''UPDATE flights_log
+        SET x = %s, y = %s, z = %s , velocity_vector_x = %s, velocity_vector_y = %s, velocity_vector_z = %s
+        WHERE flight_number = %s;'''
 
         try:
             conn = self.connection_pool.get_connection()
             curr = conn.cursor()
-            curr.execute(query)
+            values = (flight_number, x, y, z, vector_x, vector_y, vector_z)
+            curr.execute(query,values)
             conn.commit()
-            self.connection_pool.release_connection(conn)
-            msg = 'Plane position and velocity vector changed'
+            msg = {'msg': 'Plane position and velocity vector changed',
+                   'error': False}
         except psycopg2.Error as error:
             conn.rollback()
-            msg = f'Error changing plane position and velocity vector: {error}'
-            return msg
+            msg = {'msg': 'Error changing plane position and velocity vector:',
+                   'error': True,
+                   'error_msg': f"{error}"}
+        finally:
+            self.connection_pool.release_connection(conn)
+        return msg
         
 
     def num_of_planes_over_the_airport(self):
