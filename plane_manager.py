908a379348c228d23d160147d39d74cbfcc282f0
kronek <kronkowski.daniel@gmail.com>
2023-10-22 18:34:53 +0200

    change plane_signal() - related to db_manager.py, change result format to dictionary

--- plane_manager.py
+++ plane_manager.py
@@ -1,31 +1,35 @@
 '''Manage planes script'''
+from db_connection_pool import ConnectionPool
 
 class PlaneManager:
     
-    def __init__(self, dictionary, connection):
+    def __init__(self, dictionary:dict, connection):
         self.flight_number = dictionary['flight_number']
         self.position_x = dictionary['position x']
         self.position_y = dictionary['position y']
         self.position_z = dictionary['position_z']
         self.velocity = dictionary['velocity_vector']
+        self.velocity_x = self.velocity[0]
+        self.velocity_y = self.velocity[1]
+        self.velocity_z = self.velocity[2]
         self.db = connection
 
     def plane_signal(self):
         if self.position_z == 0:
             self.db.change_status(self.flight_number, 'LANDED')
-            msg = f'flight number {self.flight_number} landed'
-            print(msg)
-            result = 'landed'
+            result = {'msg':'landed',
+                      'status':'overloaded',}
         else:
             if self.db.num_of_planes_over_the_airport() < 100:
-                msg = self.db.add_plane(self.flight_number,'IN_AIR',self.position_x, self.position_y, self.position_z, self.velocity)
-                if msg == 'Error adding new plane to db':
-                    self.db.change_plane_position_and_vector(self.flight_number, self.position_x, self.position_y, self.position_z, self.velocity)
+                msg = self.db.add_plane(self.flight_number,'IN_AIR',self.position_x, self.position_y, self.position_z, self.velocity_x, self.velocity_y, self.velocity_z)
+                if msg['msg'] == 'Error adding new plane to db':
+                    self.db.change_plane_position_and_vector(self.flight_number, self.position_x, self.position_y, self.position_z, self.velocity_x, self.velocity_y, self.velocity_z)
                     result = self.control_tower_system()
                 else:
                     result = self.control_tower_system()
             else: 
-                result = 'to many planes in the air'
+                result = {'msg':'to many planes in the air',
+                          'status':'overloaded',}
         return result
 
 
@@ -36,10 +40,9 @@ class PlaneManager:
 
 
     def dictionary_data_pack(self):
-        result = {'velocity_vector':self.velocity}
+        result = {'msg':'change direction',
+                  'status':"IN_AIR",
+            'velocity_vector':self.velocity}
         return result
-
-    def close(self):
-        self.db.close_connection()
     
     
