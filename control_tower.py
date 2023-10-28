1c7474aaeed3cdac00aca34e83042271b8782d60
kronek <kronkowski.daniel@gmail.com>
2023-10-28 18:16:02 +0200

    #4 mod CONST values, add new elif for new SECTORS in guidance_system()

--- control_tower.py
+++ control_tower.py
@@ -1,11 +1,13 @@
 
 class ControlTower:
-    SECTOR_A = (-2000, 1500, 700)
-    SECTOR_B = (-2000, 1000, 600)
-    SECTOR_C = (-1000, 1000, 200)
-    SECTOR_D = (2000, -1500, 700)
-    SECTOR_E = (2000, -1000, 600)
-    SECTOR_F = (1000, -1000, 200)
+    SECTOR_A = (-2000, 1750, 500)
+    SECTOR_B = (-2000, 1000, 400)
+    SECTOR_C = (-1000, 1000, 100)
+    SECTOR_D = (2000, -1750, 500)
+    SECTOR_E = (2000, -1000, 400)
+    SECTOR_F = (1000, -1000, 100)
+    SECTOR_R1 = (1500, 1000, 0)
+    SECTOR_R2 = (-1500, -1000, 0)
 
     def __init__(self, dictionary:dict):
         self.position_x = dictionary['position_x']
@@ -35,9 +37,32 @@ class ControlTower:
             print('sektor E')
             vector = self.vector_calculation(self.SECTOR_E,(self.position_x, self.position_y, self.position_z))
 
+        elif self.position_x < -1500 and self.position_y > 0:
+            print('sektor AB')
+            vector = self.vector_calculation(self.SECTOR_C,(self.position_x, self.position_y, self.position_z))
+
+        elif self.position_x > 1500 and self.position_y < 0:
+            print('sektor ED')
+            vector = self.vector_calculation(self.SECTOR_F,(self.position_x, self.position_y, self.position_z))
+
+        elif self.position_x < -1000 and self.position_y > 0:
+            print('sektor C')
+            vector = self.vector_calculation(self.SECTOR_R1,(self.position_x, self.position_y, self.position_z))
+
+        elif self.position_x > 1000 and self.position_y < 0:
+            print('sektor F')
+            vector = self.vector_calculation(self.SECTOR_R2,(self.position_x, self.position_y, self.position_z))
+
+        elif self.position_x > 800 and self.position_y > 0:
+            print('sektor L1')
+            vector = self.vector_calculation(self.SECTOR_R1,(self.position_x, self.position_y, self.position_z))
+
+        elif self.position_x < -800 and self.position_y < 0:
+            print('sektor L2')
+            vector = self.vector_calculation(self.SECTOR_R2,(self.position_x, self.position_y, self.position_z))
+
         else:
             vector = self.dictionary_data_pack()
-
         return vector
 
     def dictionary_data_pack(self):
