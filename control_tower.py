a99249e7c7b7ac741767bd72f9efa050a01bdf05
kronek <kronkowski.daniel@gmail.com>
2023-10-28 16:57:11 +0200

    #4 change CONS values, mod guidance_system() - fix vector_calculation function

--- control_tower.py
+++ control_tower.py
@@ -1,11 +1,11 @@
 
 class ControlTower:
-    SECTOR_A = (-2499, 1999, 750)
-    SECTOR_B = (-2499, 1000, 600)
-    SECTOR_C = (-1499, 1000, 200)
-    SECTOR_D = (2499, -1999, 750)
-    SECTOR_E = (2499, -1000, 600)
-    SECTOR_F = (1499, -1000, 200)
+    SECTOR_A = (-2000, 1500, 700)
+    SECTOR_B = (-2000, 1000, 600)
+    SECTOR_C = (-1000, 1000, 200)
+    SECTOR_D = (2000, -1500, 700)
+    SECTOR_E = (2000, -1000, 600)
+    SECTOR_F = (1000, -1000, 200)
 
     def __init__(self, dictionary:dict):
         self.position_x = dictionary['position_x']
@@ -15,54 +15,62 @@ class ControlTower:
         self.velocity_x = self.velocity[0]
         self.velocity_y = self.velocity[1]
         self.velocity_z = self.velocity[2]
+        print(dictionary)
 
 
     def guidance_system(self):
         if self.position_x >= -2500 and self.position_y >= 2000:
-            distance = self.distance_calculation((self.position_x, self.position_y, self.position_z),self.SECTOR_A)
+            print('sektor A')
+            vector = self.vector_calculation(self.SECTOR_A,(self.position_x, self.position_y, self.position_z))
 
         elif self.position_x <= 2500 and self.position_y <= -2000:
-            distance = self.distance_calculation((self.position_x, self.position_y, self.position_z),self.SECTOR_D)
+            print('sektor D')
+            vector = self.vector_calculation(self.SECTOR_D,(self.position_x, self.position_y, self.position_z))
 
         elif self.position_x < -2500 and self.position_y > -2000:
-            distance = self.distance_calculation((self.position_x, self.position_y, self.position_z),self.SECTOR_B)
+            print('sektor B')
+            vector = self.vector_calculation(self.SECTOR_B,(self.position_x, self.position_y, self.position_z))
         
         elif self.position_x > 2500 and self.position_y < 2000:
-            distance = self.distance_calculation((self.position_x, self.position_y, self.position_z),self.SECTOR_E)
+            print('sektor E')
+            vector = self.vector_calculation(self.SECTOR_E,(self.position_x, self.position_y, self.position_z))
 
         else:
-            print("błędne koordynaty")
+            vector = self.dictionary_data_pack()
 
-        self.vector_change_adjustment(self.velocity, self.calc_time_at_const_speed(distance))
-        result = self.dictionary_data_pack()
-
-        return result
+        return vector
 
     def dictionary_data_pack(self):
         result = {'msg':'change direction',
                   'status':"IN_AIR",
                   'velocity_vector':self.velocity}
+        print(f'wektor: {self.velocity}')
         return result
     
 
-    def distance_calculation(self, point_one:tuple ,point_two:tuple)->float:
+    def vector_calculation(self, point_one:tuple ,point_two:tuple)->float:
         vector_x_change = point_one[0] - point_two[0]
         vector_y_change = point_one[1] - point_two[1]
         vector_z_change = point_one[2] - point_two[2]
 
         distance = (vector_x_change**2 + vector_y_change**2 + vector_z_change**2)**0.5
-        return distance
+        self.vector_change_adjustment(vector_x_change, vector_y_change, vector_z_change, self.calc_time_at_const_speed(distance))
+        print(f'dystans: {distance}')
+        result = self.dictionary_data_pack()
+        return result
     
 
     def calc_time_at_const_speed(self, distance:float)->float:
-        result = distance / 250
+        result = (distance / 250)
+        print(f'czas: {result}')
         return result
 
 
-    def vector_change_adjustment(self, vector:list, time:float):
-        self.velocity[0] = round(vector[0] / time)
-        self.velocity[1] = round(vector[1] / time)
-        self.velocity[2] = round(vector[2] / time)
+    def vector_change_adjustment(self, x:float, y:float, z:float, time:float):
+        self.velocity[0] = round(x / time)
+        self.velocity[1] = round(y / time)
+        self.velocity[2] = round(z / time)
+        print(f'składowe wektora{self.velocity}')
 
 
 
