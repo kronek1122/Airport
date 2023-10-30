9386f48d031325a806b81dcfa1c5646db6165d3a
kronek <kronkowski.daniel@gmail.com>
2023-10-30 19:39:24 +0100

    #4 add multiprocessing for starting visualization

--- main_server.py
+++ main_server.py
@@ -1,8 +1,11 @@
 from server import Server
+from multiprocessing import Process
 
 HOST = '127.0.0.1'
 PORT = 65432
 INFO = 'version: 0.0.1; creation date: 30.09.2023r'
 server = Server(HOST,PORT,INFO)
 
+animation_process = Process(target=server.visualization.run)
+animation_process.start()
 server.run()
