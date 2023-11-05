43d454cc38a6879291c2edc287bfd782553ac381
kronek <kronkowski.daniel@gmail.com>
2023-11-05 20:15:44 +0100

    #4 add information function()

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 import socket as s
 import json
 import threading
+import time
 from plane_manager import PlaneManager
 from db_manager import DatabaseManager
 from visualization import Visualization3D
@@ -24,10 +25,11 @@ class Server:
         self.db_plane_manager = DatabaseManager(**self.postgres_config)
         self.db_visualization = DatabaseManager(**self.postgres_config)
         self.visualization = Visualization3D(self.db_visualization)
+        self.db_information = DatabaseManager(**self.postgres_config)
+        
 
 
-    def handle_client(self, connection, address):
-        print(f'Connected by {address}')
+    def handle_client(self, connection):
 
         while True:
             query = connection.recv(1024).decode('utf8')
@@ -42,6 +44,15 @@ class Server:
     def run(self):
         while True:
             connection, address = self.server_socket.accept()
-            thread = threading.Thread(target=self.handle_client, args=(connection, address))
+            thread = threading.Thread(target=self.handle_client, args=(connection,))
             thread.start()
             self.active_threads +=1
+
+    def information(self):
+        while True:
+            print(f'''
+                    Samoloty w powietrzu: {self.db_information.get_num_of_planes_by_status('IN_AIR')}
+                    Wypadki: {self.db_information.get_num_of_planes_by_status('CRASHED')}
+                    Ilość udanych lądowań: {self.db_information.get_num_of_planes_by_status('LANDED')}
+                    Samoloty odprawione na inne lotnisko: {self.db_information.get_num_of_planes_by_status('REDIRECTED')}''')
+            time.sleep(10)
