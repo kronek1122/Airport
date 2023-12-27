e3865bacec54fce0e2e8ff8945af61fcd967fe6c
kronek <kronkowski.daniel@gmail.com>
2023-12-27 21:37:49 +0100

    add tests dir, add draft for test_client.py

---
+++ tests/test_client.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
+from unittest.mock import patch, MagicMock
+from client import PlaneSocket
+
+
+
